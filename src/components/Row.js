a5603cd977ecde26cde65a891efb82b04e00b532
Afsaneh <Afsaneh.heidari20@gmail.com>
2024-10-31 20:24:24 +0200

    Please check my code

---
+++ src/components/Row.js
@@ -0,0 +1,13 @@
+// src/components/Row.js
+import React from 'react';
+
+export default function Row({ item, deleteTask }) {
+  return (
+    <li key={item.id}>
+      {item.description}
+      <button className='delete-button' onClick={() => deleteTask(item.id)}>Delete</button>
+    </li>
+  );
+};
+
+
